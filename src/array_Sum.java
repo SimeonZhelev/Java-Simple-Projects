36775a1fb007319518cee3253faed2e507a41f99
Simeon Zhelev <145933339+SimeonZhelev@users.noreply.github.com>
2024-08-26 12:51:24 +0300

    Calculator

---
+++ src/array_Sum.java
@@ -0,0 +1,14 @@
+public class array_Sum {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        int sum = 0;
+
+        for (int num : array) {
+
+            sum += num;
+
+        }
+        System.out.println("Sum of elements in the array:" + sum);
+    }
+}
